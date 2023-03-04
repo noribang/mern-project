a6b328b1078b4b0c9f79b75c6a2cc0f6e7e66a82
hoonkim <hoonk9387@gmail.com>
2023-03-04 17:47:46 -0600

    Initialized package.json. Installed express. Created first express get req and res.

---
+++ server.js
@@ -0,0 +1,20 @@
+// // Import module.
+// const { application } = require('express')
+// const express = require('express')
+// // Create instance.
+// const app = express()
+
+// app.get("/", (req, res) => {
+//     res.send("Welcome to the")
+// })
+
+// app.listen(4000)
+
+const express = require('express')
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the first page.")
+})
+
+app.listen(4000)
