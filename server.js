320c3312145188443eec83624d93a844df10d452
hoonkim <hoonk9387@gmail.com>
2023-03-04 20:32:46 -0600

    Connected to db in server file.

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 // Import mongodb driver module.
 // Opens connection to db.
 const {MongoClient} = require('mongodb')
-
-
 const express = require('express')
 const app = express()
+
+let db
 
 app.get("/", (req, res) => {
     res.send("Welcome to the first page.")
@@ -14,4 +14,13 @@ app.get("/admin", function(req, res) {
     res.send("This is the admin page.")
 })
 
-app.listen(4000)
+// Connect to db.
+// Async returns Promise.
+async function start() {
+    const client = new MongoClient("mongodb://root:root@localhost:27017/AmazingMernApp?&authSource=admin")
+    await client.connect()
+    // Returns db.
+    db = client.db()
+    app.listen(4000)    
+}
+start()
