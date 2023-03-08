8c08d7a3a6e3576e68eabbba66d1e0bd62cd4943
hoonkim <hoonk9387@gmail.com>
2023-03-07 22:29:28 -0600

    Started React in index.js.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert("Testing----")
+import React from 'react'
+import {createRoot} from 'react-dom/client'
+
+function App() {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <p>Hey, from React...</p>
+        </div>
+    )
+}
+
+const root = createRoot(document.querySelector("#app"))
+root.render(<App />)
