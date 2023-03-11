0da6258023480e3df1d90561b47732d67b0b7e88
hoonkim <hoonk9387@gmail.com>
2023-03-10 22:02:06 -0600

    Starting middleware function for password protection.

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ app.set("views,", "./views")
 // Middleware access public directory with main.js.
 app.use(express.static("public"))
 
+// Middleware function for password protection.
+function passwordProtected(req, res, next) {
+    res.set("WWW-Authenticate", "Basic realm='Our MERN App'")
+}
+
 app.get("/", async (req, res) => {
     const allAnimals = await db.collection("animals").find().toArray() 
     // console.log(allAnimals)
